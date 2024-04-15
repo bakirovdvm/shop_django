62a8ca612e2cdf1f45bf907fadf2e1942bdac6d5
sergali_bakirov <bakirovdvm@gmail.com>
2024-04-15 23:55:34 +0500

    change password is work!

--- app_my_shop/my_profile/urls.py
+++ app_my_shop/my_profile/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, PasswordView
 
 
 app_name = "my_profile"
 
 urlpatterns = [
-    path("profile/", ProfileView.as_view(), name="profile")
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/password", PasswordView.as_view(), name="changePassword"),
 ]
--- app_my_shop/my_profile/views.py
+++ app_my_shop/my_profile/views.py
@@ -3,7 +3,7 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Profile
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, ChangePasswordSerializer
 
 
 class ProfileView(APIView):
@@ -21,3 +21,26 @@ class ProfileView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PasswordView(APIView):
+    def post(self, request):
+        print(request)
+        print(request.data)
+        serializer = ChangePasswordSerializer(data=request.data)
+
+        if serializer.is_valid():
+            current_password = serializer.validated_data['currentPassword']
+            new_password = serializer.validated_data['newPassword']
+
+            user = request.user
+
+            if user.check_password(current_password):
+                user.set_password(new_password)
+                user.save()
+                return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Invalid current password"}, status=status.HTTP_400_BAD_REQUEST,)
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
