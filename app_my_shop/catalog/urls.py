8e9dff39ce70f971496e8daca19720d06ae25dda
sergali_bakirov <bakirovdvm@gmail.com>
2024-04-25 23:49:56 +0500

    product and catalog work need to prepare and fix nrxt step

--- app_my_shop/catalog/urls.py
+++ app_my_shop/catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import CategoriesView
 
 
 app_name = "catalog"
 
 urlpatterns = [
     # path('product/<int:product_id>/', ProductView.as_view(), name='product')
+    path('categories/', CategoriesView.as_view(), name='categories')
 ]
--- app_my_shop/catalog/views.py
+++ app_my_shop/catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import CategorySerializer, SubCategorySerializer
+from .models import Category, Subcategory
 
-# Create your views here.
+
+class CategoriesView(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        category_data = list()
+        for category in categories:
+            print('pk'.upper(), category.pk)
+            print('category'.upper(), category.title)
+            print('image'.upper(), category.get_image())
+
+
+            subcategories = Subcategory.objects.all()
+            for subcategory in subcategories:
+                print('pk'.upper(), subcategory.pk)
+                print('category'.upper(), subcategory.title)
+                print('image'.upper(), subcategory.get_image())
