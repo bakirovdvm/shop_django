1ea4797cddc61cd29fbc10dba91ffb067525ac9b
sergali_bakirov <bakirovdvm@gmail.com>
2024-05-01 23:59:20 +0500

    product review is work! i can see old reviews and i can create new!

--- app_my_shop/product/urls.py
+++ app_my_shop/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductView
+from .views import ProductView, ProductReview
 
 app_name = "product"
 
 urlpatterns = [
-    path('product/<int:product_id>/', ProductView.as_view(), name='product')
+    path('product/<int:product_id>/', ProductView.as_view(), name='product'),
+    path('product/<int:id>/reviews', ProductReview.as_view(), name='product_review'),
 ]
--- app_my_shop/product/views.py
+++ app_my_shop/product/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductReviewSerializer
 from .models import Product
 
 
@@ -13,16 +14,27 @@ class ProductView(APIView):
         try:
             product = Product.objects.get(pk=product_id)
             serialiaer = ProductSerializer(product)
-            print(serialiaer.data)
+            # print(serialiaer.data)
             # serialiaer.data['category'] = 1
-            print(serialiaer.data)
+            # print(serialiaer.data)
             return Response(serialiaer.data)
         except Product.DoesNotExist:
             return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        # print(request)
-        # print(request.data)
-        # print(product_id)
-        # print(request.user)
 
+class ProductReview(APIView):
+    def get(self, request, id):
+        reviews = ProductReview.objects.filter(pk=id)
+        serializer = ProductReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, id):
+        product = get_object_or_404(Product, pk=id)
+        request.data['product'] = id
+        serializer = ProductReviewSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
