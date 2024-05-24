cd514e1b8477d2174819ecc304064269ff0c3e59
sergali_bakirov <bakirovdvm@gmail.com>
2024-05-25 00:23:18 +0500

    baskets DELETE dont work,,, need to fix if

--- app_my_shop/basket/views.py
+++ app_my_shop/basket/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from .serializers import BasketSerializer, BasketItemSerializer
 from .models import Basket, BasktetItem
 from product.models import Product
+from .serializers import BasketItemSerializer
 
 
 class BasketView(APIView):
@@ -60,8 +61,37 @@ class BasketView(APIView):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    def delete(self, request):
+        product_id = request.data.get('id')
+        quantity = request.data.get('count')
+
+        print(product_id, quantity)
+
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return Response({'error': 'product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if request.data.user.is_authenticated:
+            basket, created = Basket.objects.get_or_create(user=request.user)
+        else:
+            session_key = request.session.session_key
+            if not session_key:
+                request.session.create()
+                session_key = request.session.session_key
+
+            basket, created = Basket.objects.get_or_create(session_key=session_key)
+
+        basket_item, created = BasktetItem.objects.get_or_create(basket=basket, product=product)
 
 
+        if basket_item.quantity > quantity:
+            basket_item.quantity -= int(quantity)
 
+            basket_item.save()
+        else:
+            basket_item.delete()
 
+        serializer = BasketItemSerializer(BasketSerializer)
 
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
